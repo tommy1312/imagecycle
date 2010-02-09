1aee49f105b3c225b2d3102f39dfbc681597b4b7
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-09 21:44:52 +0000

    Bugfix typoscript, alt="TEXT" if no caption was defined

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "imagecycle".
 #
-# Auto generated 07-02-2010 21:04
+# Auto generated 09-02-2010 22:43
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.2.1',
+	'version' => '0.2.2',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:23:{s:23:"class.tx_imagecycle.php";s:4:"dc4a";s:12:"ext_icon.gif";s:4:"7990";s:17:"ext_localconf.php";s:4:"0f34";s:14:"ext_tables.php";s:4:"8697";s:14:"ext_tables.sql";s:4:"43cf";s:15:"flexform_ds.xml";s:4:"c4a5";s:13:"locallang.xml";s:4:"a63a";s:16:"locallang_db.xml";s:4:"7f0f";s:12:"t3jquery.txt";s:4:"b484";s:14:"doc/manual.sxw";s:4:"3589";s:19:"doc/wizard_form.dat";s:4:"2930";s:20:"doc/wizard_form.html";s:4:"b3a6";s:14:"pi1/ce_wiz.gif";s:4:"7667";s:31:"pi1/class.tx_imagecycle_pi1.php";s:4:"e2a4";s:39:"pi1/class.tx_imagecycle_pi1_wizicon.php";s:4:"2e15";s:17:"pi1/locallang.xml";s:4:"48b7";s:33:"res/jquery/js/jquery-1.3.2.min.js";s:4:"bb38";s:33:"res/jquery/js/jquery-1.4.0.min.js";s:4:"9e93";s:33:"res/jquery/js/jquery-1.4.1.min.js";s:4:"0d40";s:37:"res/jquery/js/jquery.cycle.all.min.js";s:4:"f813";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"5d8f";s:16:"static/setup.txt";s:4:"ed3e";}',
+	'_md5_values_when_last_written' => 'a:23:{s:23:"class.tx_imagecycle.php";s:4:"dc4a";s:12:"ext_icon.gif";s:4:"7990";s:17:"ext_localconf.php";s:4:"0f34";s:14:"ext_tables.php";s:4:"8697";s:14:"ext_tables.sql";s:4:"43cf";s:15:"flexform_ds.xml";s:4:"c4a5";s:13:"locallang.xml";s:4:"a63a";s:16:"locallang_db.xml";s:4:"7f0f";s:12:"t3jquery.txt";s:4:"b484";s:14:"doc/manual.sxw";s:4:"1d3b";s:19:"doc/wizard_form.dat";s:4:"2930";s:20:"doc/wizard_form.html";s:4:"b3a6";s:14:"pi1/ce_wiz.gif";s:4:"7667";s:31:"pi1/class.tx_imagecycle_pi1.php";s:4:"e2a4";s:39:"pi1/class.tx_imagecycle_pi1_wizicon.php";s:4:"2e15";s:17:"pi1/locallang.xml";s:4:"48b7";s:33:"res/jquery/js/jquery-1.3.2.min.js";s:4:"bb38";s:33:"res/jquery/js/jquery-1.4.0.min.js";s:4:"9e93";s:33:"res/jquery/js/jquery-1.4.1.min.js";s:4:"0d40";s:37:"res/jquery/js/jquery.cycle.all.min.js";s:4:"f813";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"5d8f";s:16:"static/setup.txt";s:4:"7c39";}',
 	'suggests' => array(
 	),
 );
