69bb0cb25e80d8c39d1c738e5b964656025b3800
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-29 21:02:19 +0000

    Just textual changes (No functional changes)

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "imagecycle".
 #
-# Auto generated 24-03-2010 18:33
+# Auto generated 29-03-2010 23:01
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.8.0',
+	'version' => '0.8.1',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:25:{s:23:"class.tx_imagecycle.php";s:4:"86ee";s:21:"ext_conf_template.txt";s:4:"af60";s:12:"ext_icon.gif";s:4:"7990";s:17:"ext_localconf.php";s:4:"d485";s:14:"ext_tables.php";s:4:"7093";s:14:"ext_tables.sql";s:4:"8875";s:15:"flexform_ds.xml";s:4:"7f12";s:13:"locallang.xml";s:4:"5ce3";s:16:"locallang_db.xml";s:4:"18af";s:12:"t3jquery.txt";s:4:"8766";s:14:"doc/manual.sxw";s:4:"a8db";s:41:"lib/class.tx_imagecycle_itemsProcFunc.php";s:4:"7113";s:14:"pi1/ce_wiz.gif";s:4:"7667";s:31:"pi1/class.tx_imagecycle_pi1.php";s:4:"c6a5";s:39:"pi1/class.tx_imagecycle_pi1_wizicon.php";s:4:"2e15";s:17:"pi1/locallang.xml";s:4:"14d7";s:33:"res/jquery/js/jquery-1.3.2.min.js";s:4:"bb38";s:33:"res/jquery/js/jquery-1.4.0.min.js";s:4:"9e93";s:33:"res/jquery/js/jquery-1.4.1.min.js";s:4:"0d40";s:33:"res/jquery/js/jquery-1.4.2.min.js";s:4:"1009";s:42:"res/jquery/js/jquery.cycle.all.min-2.75.js";s:4:"d398";s:42:"res/jquery/js/jquery.cycle.all.min-2.80.js";s:4:"0975";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"b481";s:16:"static/setup.txt";s:4:"41ce";}',
+	'_md5_values_when_last_written' => 'a:25:{s:23:"class.tx_imagecycle.php";s:4:"86ee";s:21:"ext_conf_template.txt";s:4:"af60";s:12:"ext_icon.gif";s:4:"7990";s:17:"ext_localconf.php";s:4:"d485";s:14:"ext_tables.php";s:4:"7093";s:14:"ext_tables.sql";s:4:"8875";s:15:"flexform_ds.xml";s:4:"c711";s:13:"locallang.xml";s:4:"5ce3";s:16:"locallang_db.xml";s:4:"18af";s:12:"t3jquery.txt";s:4:"8766";s:14:"doc/manual.sxw";s:4:"61b5";s:41:"lib/class.tx_imagecycle_itemsProcFunc.php";s:4:"0b2a";s:14:"pi1/ce_wiz.gif";s:4:"7667";s:31:"pi1/class.tx_imagecycle_pi1.php";s:4:"c6a5";s:39:"pi1/class.tx_imagecycle_pi1_wizicon.php";s:4:"2e15";s:17:"pi1/locallang.xml";s:4:"14d7";s:33:"res/jquery/js/jquery-1.3.2.min.js";s:4:"bb38";s:33:"res/jquery/js/jquery-1.4.0.min.js";s:4:"9e93";s:33:"res/jquery/js/jquery-1.4.1.min.js";s:4:"0d40";s:33:"res/jquery/js/jquery-1.4.2.min.js";s:4:"1009";s:42:"res/jquery/js/jquery.cycle.all.min-2.75.js";s:4:"d398";s:42:"res/jquery/js/jquery.cycle.all.min-2.80.js";s:4:"0975";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"b481";s:16:"static/setup.txt";s:4:"41ce";}',
 	'suggests' => array(
 	),
 );
