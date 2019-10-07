5cfa5e382ddc136ea83ca27980b9763eb480f5d2
Franz Holzinger <holzinger@ttproducts.de>
2019-10-07 14:53:18 +0200

    add files ChangeLog and ext_autoload.php

---
+++ ext_autoload.php
@@ -0,0 +1,19 @@
+<?php
+$emClass = '\\TYPO3\\CMS\\Core\\Utility\\ExtensionManagementUtility';
+
+$key = 'imagecycle';
+$extensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($key, $script);
+
+return array(
+    'tx_imagecycle_cms_layout' => $extensionPath . 'lib/class.tx_imagecycle_cms_layout.php',
+    'tx_imagecycle_itemsProcFunc' => $extensionPath . 'lib/class.tx_imagecycle_itemsProcFunc.php',
+    'tx_imagecycle_pagerenderer' => $extensionPath . 'lib/class.tx_imagecycle_pagerenderer.php',
+    'tx_imagecycle_TCAform' => $extensionPath . 'lib/class.tx_imagecycle_TCAform.php',
+    'tx_imagecycle_tsparserext' => $extensionPath . 'lib/class.tx_imagecycle_tsparserext.php',
+    'tx_imagecycle_pi1' => $extensionPath . 'pi1/class.tx_imagecycle_pi1.php',
+    'tx_imagecycle_pi2' => $extensionPath . 'pi2/class.tx_imagecycle_pi2.php',
+    'tx_imagecycle_pi3' => $extensionPath . 'pi3/class.tx_imagecycle_pi3.php',
+    'tx_imagecycle_pi4' => $extensionPath . 'pi4/class.tx_imagecycle_pi4.php',
+    'tx_imagecycle_pi5' => $extensionPath . 'pi5/class.tx_imagecycle_pi5.php',
+);
+
