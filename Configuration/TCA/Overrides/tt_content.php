19413427ae197a227973215cf3f464bb7a4fb86a
Franz Holzinger <holzinger@ttproducts.de>
2019-10-15 16:16:53 +0200

    fix relative path to the plugin images

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,20 +3,20 @@ defined('TYPO3_MODE') || die('Access denied.');
 
 $table = 'tt_content';
 
-$relativeExtensionPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('imagecycle');
+$relativeExtensionPath = 'EXT:' . IMAGECYLCE_EXT . '/'; 
 
 // CONTENT
 $tempColumns = array(
     'tx_imagecycle_activate' => array(
         'exclude' => 1,
-        'label' => 'LLL:EXT:imagecycle/locallang_db.xml:' . $table . '.tx_imagecycle_activate',
+        'label' => 'LLL:EXT:' . IMAGECYLCE_EXT . '/locallang_db.xml:' . $table . '.tx_imagecycle_activate',
         'config' => array(
             'type' => 'check',
         )
     ),
     'tx_imagecycle_duration' => array(
         'exclude' => 1,
-        'label' => 'LLL:EXT:imagecycle/locallang_db.xml:' . $table . '.tx_imagecycle_duration',
+        'label' => 'LLL:EXT:' . IMAGECYLCE_EXT . '/locallang_db.xml:' . $table . '.tx_imagecycle_duration',
         'config' => array(
             'type' => 'input',
             'size' => '5',
@@ -31,23 +31,24 @@ $GLOBALS['TCA'][$table]['palettes']['tx_imagecycle'] = array(
     'showitem' => 'tx_imagecycle_activate,tx_imagecycle_duration',
     'canNotCollapse' => 1,
 );
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($table, '--palette--;LLL:EXT:imagecycle/locallang_db.xml:' . $table . '.tx_imagecycle_title;tx_imagecycle', 'textpic,image', 'before:imagecaption');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($table, '--palette--;LLL:EXT:' . IMAGECYLCE_EXT . '/locallang_db.xml:' . $table . '.tx_imagecycle_title;tx_imagecycle', 'textpic,image', 'before:imagecaption');
 
 $listType = 'imagecycle_pi1';
 
 // ICON pi1
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
     array(
-        'LLL:EXT:imagecycle/locallang_db.xml:' . $table . '.list_type_pi1',
+        'LLL:EXT:' . IMAGECYLCE_EXT . '/locallang_db.xml:' . $table . '.list_type_pi1',
         $listType,
         $relativeExtensionPath . 'pi1/ce_icon.gif'
     ),
     'list_type',
-    'imagecycle'
+    IMAGECYLCE_EXT
 );
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     $listType,
-    'FILE:EXT:imagecycle/pi1/flexform_ds.xml'
+    'FILE:EXT:' . IMAGECYLCE_EXT . '/pi1/flexform_ds.xml'
 );
 
 $listType = 'imagecycle_pi2';
@@ -55,16 +56,16 @@ $listType = 'imagecycle_pi2';
 // ICON pi2
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
     array(
-        'LLL:EXT:imagecycle/locallang_db.xml:' . $table . '.list_type_pi2', 
+        'LLL:EXT:' . IMAGECYLCE_EXT . '/locallang_db.xml:' . $table . '.list_type_pi2', 
         $listType,
         $relativeExtensionPath . 'pi2/ce_icon.gif'
     ),
     'list_type',
-    'imagecycle'
+    IMAGECYLCE_EXT
 );
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     $listType,
-    'FILE:EXT:imagecycle/pi2/flexform_ds.xml'
+    'FILE:EXT:' . IMAGECYLCE_EXT . '/pi2/flexform_ds.xml'
 );
 
 $listType = 'imagecycle_pi3';
@@ -77,11 +78,11 @@ $listType = 'imagecycle_pi3';
         $relativeExtensionPath . 'pi3/ce_icon.gif'
     ),
     'list_type',
-    'imagecycle'
+    IMAGECYLCE_EXT
 );
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     $listType, 
-    'FILE:EXT:imagecycle/pi3/flexform_ds.xml'
+    'FILE:EXT:' . IMAGECYLCE_EXT . '/pi3/flexform_ds.xml'
 );
 
 $listType = 'imagecycle_pi4';
@@ -89,16 +90,16 @@ $listType = 'imagecycle_pi4';
 // ICON pi4
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
     array(
-        'LLL:EXT:imagecycle/locallang_db.xml:' . $table . '.list_type_pi4',
+        'LLL:EXT:' . IMAGECYLCE_EXT . '/locallang_db.xml:' . $table . '.list_type_pi4',
         $listType,
         $relativeExtensionPath . 'pi4/ce_icon.gif'
     ),
     'list_type',
-    'imagecycle'
+    IMAGECYLCE_EXT
 );
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     $listType,
-    'FILE:EXT:imagecycle/pi4/flexform_ds.xml'
+    'FILE:EXT:' . IMAGECYLCE_EXT .'/pi4/flexform_ds.xml'
 );
 
 $listType = 'imagecycle_pi5';
@@ -106,16 +107,16 @@ $listType = 'imagecycle_pi5';
 // ICON pi5
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(
     array(
-        'LLL:EXT:imagecycle/locallang_db.xml:' . $table . '.list_type_pi5',
+        'LLL:EXT:' . IMAGECYLCE_EXT . '/locallang_db.xml:' . $table . '.list_type_pi5',
         $listType,
         $relativeExtensionPath . 'pi5/ce_icon.gif'
     ),
     'list_type',
-    'imagecycle'
+    IMAGECYLCE_EXT
 );
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
     $listType,
-    'FILE:EXT:imagecycle/pi5/flexform_ds.xml'
+    'FILE:EXT:' . IMAGECYLCE_EXT . '/pi5/flexform_ds.xml'
 );
 
 $GLOBALS['TCA'][$table]['types']['list']['subtypes_excludelist']['imagecycle_pi1'] = 'layout,select_key,pages';
